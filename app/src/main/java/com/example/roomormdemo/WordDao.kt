a3688c7e00a1d1f4b5ad7c4d2bf360b325c44996
Christopherson <achristopherson@Galaxe.com>
2021-12-14 13:54:13 -0600

    wrapping the List<Word> in a Flow to produce values one at a time for asyncronous operations

--- app/src/main/java/com/example/roomormdemo/WordDao.kt
+++ app/src/main/java/com/example/roomormdemo/WordDao.kt
@@ -4,12 +4,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * importing this Flow will cause error
+ * import java.util.concurrent.Flow
+ */
 
 @Dao
 interface WordDao {
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAlphabetizedWords(): List<Word>
+    fun getAlphabetizedWords(): Flow<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(word: Word)
