392f98719d4e12fca53ec8a2f0095e174cc54398
Christopherson <achristopherson@Galaxe.com>
2021-12-15 09:08:11 -0600

    added callback function to populate the database

--- app/src/main/java/com/example/roomormdemo/Word.kt
+++ app/src/main/java/com/example/roomormdemo/Word.kt
@@ -6,7 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "word_table")
 data class Word(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey
+//        (autoGenerate = true) val id: Int,
     @ColumnInfo(name = "word") val word: String
     ) {
 
